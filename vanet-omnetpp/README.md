842e23ce585a4b39e784efafa3d5af453725b705
Rida Saghir <ridasagheer94@gmail.com>
2023-01-02 17:00:18 +0100

    Add files via upload
    Omnetpp vanet project

---
+++ vanet-omnetpp/README.md
@@ -0,0 +1,7 @@
+This OMNET++ project is best compiled on following versions:
+
+OMNET++ : version 5.6.2
+Veins: https://github.com/sommer/veins
+Inet: https://github.com/inet-framework/inet
+
+This project includes a small map of an area from Bremen, Germany which includes routes for almost 1000 vehicles.
---
+++ vanet-omnetpp/out/gcc-debug/src/veins_inet/VeinsInetMobility.o.d
@@ -0,0 +1,195 @@
+../out/gcc-debug/src/veins_inet/VeinsInetMobility.o: \
+ veins_inet/VeinsInetMobility.cc veins_inet/VeinsInetMobility.h \
+ ../../inet/src/inet/mobility/base/MobilityBase.h \
+ ../../inet/src/inet/common/ModuleAccess.h \
+ ../../inet/src/inet/common/INETDefs.h \
+ ../../inet/src/inet/common/precompiled_debug.h \
+ ../../inet/src/inet/common/precompiled.h \
+ /home/rida/omnetpp-5.6.2/include/omnetpp.h \
+ ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+ ../../inet/src/inet/common/Compat.h \
+ ../../inet/src/inet/common/InitStages.h \
+ ../../inet/src/inet/networklayer/contract/IInterfaceTable.h \
+ ../../inet/src/inet/networklayer/common/InterfaceEntry.h \
+ ../../inet/src/inet/common/packet/tag/TagSet.h \
+ ../../inet/src/inet/common/Simsignals.h \
+ ../../inet/src/inet/common/Simsignals_m.h \
+ ../../inet/src/inet/common/INETDefs_m.h \
+ ../../inet/src/inet/linklayer/common/MacAddress.h \
+ ../../inet/src/inet/networklayer/common/InterfaceToken.h \
+ ../../inet/src/inet/networklayer/common/L3Address.h \
+ ../../inet/src/inet/networklayer/common/ModuleIdAddress.h \
+ ../../inet/src/inet/networklayer/common/ModulePathAddress.h \
+ ../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h \
+ ../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+ ../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+ ../../inet/src/inet/common/StringFormat.h \
+ ../../inet/src/inet/common/geometry/common/CanvasProjection.h \
+ ../../inet/src/inet/common/geometry/common/RotationMatrix.h \
+ ../../inet/src/inet/common/geometry/common/Coord.h \
+ ../../inet/src/inet/common/INETMath.h \
+ ../../inet/src/inet/common/geometry/common/EulerAngles.h \
+ ../../inet/src/inet/common/Units.h \
+ ../../inet/src/inet/common/geometry/common/Quaternion.h \
+ ../../inet/src/inet/mobility/contract/IMobility.h \
+ veins_inet/veins_inet.h \
+ /home/rida/veins-master/veins-master/src/veins/veins.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/Coord.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/FWMath.h \
+ /home/rida/veins-master/veins-master/src/veins/base/modules/BaseWorldUtility.h \
+ /home/rida/veins-master/veins-master/src/veins/base/connectionManager/BaseConnectionManager.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/AntennaPosition.h \
+ /home/rida/veins-master/veins-master/src/veins/base/connectionManager/NicEntry.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/Heading.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/utility/HasLogProxy.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/FindModule.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/obstacle/ObstacleControl.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/obstacle/Obstacle.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/world/annotations/AnnotationManager.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/utility/BBoxLookup.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/obstacle/VehicleObstacleControl.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/Move.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/obstacle/MobileHostObstacle.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIBuffer.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIConstants.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIColor.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIConnection.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICoord.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/VehicleSignal.h \
+ /home/rida/veins-master/veins-master/src/veins/base/utils/EnumBitset.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+ /home/rida/veins-master/veins-master/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+ ../../inet/src/inet/common/geometry/common/GeographicCoordinateSystem.h
+
+veins_inet/VeinsInetMobility.h:
+
+../../inet/src/inet/mobility/base/MobilityBase.h:
+
+../../inet/src/inet/common/ModuleAccess.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_debug.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+/home/rida/omnetpp-5.6.2/include/omnetpp.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../inet/src/inet/networklayer/contract/IInterfaceTable.h:
+
+../../inet/src/inet/networklayer/common/InterfaceEntry.h:
+
+../../inet/src/inet/common/packet/tag/TagSet.h:
+
+../../inet/src/inet/common/Simsignals.h:
+
+../../inet/src/inet/common/Simsignals_m.h:
+
+../../inet/src/inet/common/INETDefs_m.h:
+
+../../inet/src/inet/linklayer/common/MacAddress.h:
+
+../../inet/src/inet/networklayer/common/InterfaceToken.h:
+
+../../inet/src/inet/networklayer/common/L3Address.h:
+
+../../inet/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../../inet/src/inet/networklayer/common/ModulePathAddress.h:
+
+../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h:
+
+../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../../inet/src/inet/common/StringFormat.h:
+
+../../inet/src/inet/common/geometry/common/CanvasProjection.h:
+
+../../inet/src/inet/common/geometry/common/RotationMatrix.h:
+
+../../inet/src/inet/common/geometry/common/Coord.h:
+
+../../inet/src/inet/common/INETMath.h:
+
+../../inet/src/inet/common/geometry/common/EulerAngles.h:
+
+../../inet/src/inet/common/Units.h:
+
+../../inet/src/inet/common/geometry/common/Quaternion.h:
+
+../../inet/src/inet/mobility/contract/IMobility.h:
+
+veins_inet/veins_inet.h:
+
+/home/rida/veins-master/veins-master/src/veins/veins.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/Coord.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/FWMath.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/modules/BaseWorldUtility.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/AntennaPosition.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/connectionManager/NicEntry.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/Heading.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/utility/HasLogProxy.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/FindModule.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/obstacle/ObstacleControl.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/obstacle/Obstacle.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/world/annotations/AnnotationManager.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/utility/BBoxLookup.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/Move.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIColor.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICoord.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+/home/rida/veins-master/veins-master/src/veins/base/utils/EnumBitset.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+/home/rida/veins-master/veins-master/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+../../inet/src/inet/common/geometry/common/GeographicCoordinateSystem.h:
